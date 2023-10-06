f98b68883385280ac694db1d44cc8cff60f45357
pauloborgesgh <pauloborgeszambon@gmail.com>
2023-10-06 11:57:48 -0300

    Update tabelas_banco_faculdade.sql

--- tabelas_banco_faculdade.sql
+++ tabelas_banco_faculdade.sql
@@ -44,33 +44,29 @@ foreign key (idaluno)references Aluno (idaluno) ,
 foreign key (iddisciplina)references Disciplina (iddisciplina)
 );
 
-ChatGPT
-Vou fornecer as consultas SQL para cada uma das suas solicitações:
 
-2. Consulta para listar o quantitativo de cursos existentes:
 
-sql
-Copy code
+/*2. Consulta para listar o quantitativo de cursos existentes:*/
+
+
 SELECT COUNT(*) AS QuantidadeCursos
 FROM Curso;
-Esta consulta retornará o número total de cursos existentes.
+/*Esta consulta retornará o número total de cursos existentes.*/
 
-4. Consulta para listar o nome de todos os cursos e seus respectivos alunos em ordem decrescente pelo nome dos cursos:
+/*4. Consulta para listar o nome de todos os cursos e seus respectivos alunos em ordem decrescente pelo nome dos cursos:*/
 
-sql
-Copy code
+
 SELECT Curso.nome AS NomeCurso, GROUP_CONCAT(Aluno.nome ORDER BY Aluno.nome DESC SEPARATOR ', ') AS Alunos
 FROM Curso
 LEFT JOIN AlunoCurso ON Curso.idCurso = AlunoCurso.idCurso
 LEFT JOIN Aluno ON AlunoCurso.idaluno = Aluno.idaluno
 GROUP BY Curso.nome
 ORDER BY NomeCurso DESC;
-Esta consulta irá listar o nome de todos os cursos e seus respectivos alunos em ordem decrescente pelo nome dos cursos. Os nomes dos alunos estão agrupados em uma única coluna chamada "Alunos".
+/*Esta consulta irá listar o nome de todos os cursos e seus respectivos alunos em ordem decrescente pelo nome dos cursos. Os nomes dos alunos estão agrupados em uma única coluna chamada "Alunos".*/
 
-5. Consulta para listar a média das notas das disciplinas de todos os cursos:
+/*5. Consulta para listar a média das notas das disciplinas de todos os cursos:*/
 
-sql
-Copy code
+
 SELECT Curso.nome AS NomeCurso, AVG(Historico.nota) AS MediaNotas
 FROM Curso
 LEFT JOIN Grade ON Curso.idCurso = Grade.idCurso
@@ -78,7 +74,7 @@ LEFT JOIN GradeDisciplina ON Grade.idGrade = GradeDisciplina.idGrade
 LEFT JOIN Disciplina ON GradeDisciplina.iddisciplina = Disciplina.iddisciplina
 LEFT JOIN Historico ON Disciplina.iddisciplina = Historico.iddisciplina
 GROUP BY Curso.nome;
-Esta consulta irá listar o nome de todos os cursos e a média das notas das disciplinas de cada curso.
+ /*Esta consulta irá listar o nome de todos os cursos e a média das notas das disciplinas de cada curso.*/
 
 6. Consulta para listar o nome de todos os cursos e a quantidade de alunos em cada curso:
 
@@ -88,9 +84,8 @@ SELECT Curso.nome AS NomeCurso, COUNT(AlunoCurso.idaluno) AS QuantidadeAlunos
 FROM Curso
 LEFT JOIN AlunoCurso ON Curso.idCurso = AlunoCurso.idCurso
 GROUP BY Curso.nome;
-Esta consulta irá listar o nome de todos os cursos e a quantidade de alunos em cada curso.
+/*Esta consulta irá listar o nome de todos os cursos e a quantidade de alunos em cada curso.*/
 
-Certifique-se de executar essas consultas no contexto do seu banco de dados Faculdade.
 
 
 
